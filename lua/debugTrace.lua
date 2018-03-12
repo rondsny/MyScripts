13579bb64d49decb55c2ae0d1816c69b3c880caf
Ron Ngai <rondsny@gmail.com>
2018-03-12 18:51:05 +0800

    Update debugTrace.lua
    1. add sample
    2. string format

--- lua/debugTrace.lua
+++ lua/debugTrace.lua
@@ -6,12 +6,16 @@ traceXXX = function () end
 -- debug
 traceXXX = not bDebug and function () end or
     function (...)
-        local tbInfo = debug.getinfo(2)
-        local dLine  = tbInfo.currentline
-        local source = tbInfo.source
-        local mod    = string.match(source, "/([_%w]+).lua$")
+        local tbInfo   = debug.getinfo(2)
+        local funcName = tbInfo.name
+        local dLine    = tbInfo.currentline
+        local source   = tbInfo.source
+        local mod      = string.match(source, "/([_%w]+).lua$")
         if not mod then mod = "" end
-        local preMsg = string.format("[XXX] %s:%s", mod, dLine)
+        local preMsg = string.format("[XXX] %s:%s %s", mod, dLine, funcName)
         
         print(preMsg, ...)
     end
+
+-- e.g.
+-- [XXX] myMod:6 test ...
