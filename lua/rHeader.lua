c3a54659a5dc907a2d8ea39e900a3be7374e5d11
Ron Ngai <rondsny@gmail.com>
2018-05-17 13:30:09 +0800

    Create rHeader.lua

---
+++ lua/rHeader.lua
@@ -0,0 +1,10 @@
+-- @Author: weiyanguang
+-- @Date: 2018-05-15 17:34:20
+-- @Last Modified by: weiyanguang
+-- @Last Modified time: 2018-05-15 17:36:27
+-- @Doc:
+
+local le_pre = le_pre or ""
+
+require(le_pre.."rUtil")
+require(le_pre.."rStruct")
